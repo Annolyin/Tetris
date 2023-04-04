4df5f845e691096e8da6c8807ef37fa48b7a3c72
Annolyin <Annolyin@gmail.com>
2023-04-04 20:23:38 +1000

    Base working
    Just need to add score and then different pieces

--- Tetris/src/main.cpp
+++ Tetris/src/main.cpp
@@ -2,58 +2,61 @@
 #include "raylib.h"
 #include <iostream>
 
-//pieces
-const int pieceWidth = 5;
-const int pieceHeight = 5;
-
-int piece[pieceHeight][pieceWidth] =
-{
-    {0, 0, 0, 0, 0},
-    {0, 0, 1, 0, 0},
-    {0, 0, 1, 0, 0},
-    {0, 1, 1, 0, 0},
-    {0, 0, 0, 0, 0}
-};
-
-int piecePosX = -1;
-int piecePosY = -1;
-
+//forward declare functions
 void AddPieceToBoard(int xPos, int yPos);
 void RemovePiece(int xPos, int yPos);
+void PrintPiece();
 void DrawBoard();
 void MoveDown();
 void MoveLeft();
 void MoveRight();
-void CheckFullRow();
+bool CheckFullRow();
 bool CheckMoveDown();
 bool CheckMoveRight();
 bool CheckMoveLeft();
 void RotateRight();
-void RotateLeft();
 
-//board
+/*
+ * Pieces
+ */
 
+//height/width of piece
+const int pieceWidth = 5;
+const int pieceHeight = 5;
+//array to hold current piece
+int piece[pieceHeight][pieceWidth] =
+    {
+        {0, 0, 0, 0, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+//position of current piece
+int piecePosX = -1;
+int piecePosY = -1;
+
+
+/*
+ * Board 
+ */
+
+//board size
 const int bCols = 10;
 const int bRows = 20;
-
+//tile size
 int tileWidth = 20;
 int tileHeight = 20;
-
+//window size
 int windowWidth = bCols * tileWidth;
 int windowHeight = bRows * tileHeight;
-
+//board array
 char board[bRows][bCols];
 
 
 void main()
 {
-    //TestPieces();
-    // Initialization
-    //--------------------------------------------------------------------------------------
-
-
-    //initialise board array
-    std::cout << "initialise array \n";
+    //Initialise board array with 0s
     for (int r = 0; r < bRows; r++)
     {
         for (int c = 0; c < bCols; c++)
@@ -62,55 +65,52 @@ void main()
         }
     }
 
-
-    std::cout << "initialising window with" << windowWidth<< "x" <<windowHeight << "\n";
+    //Setup window
     InitWindow(windowWidth, windowHeight, "Tetris");
+    SetTargetFPS(30);
 
-    SetTargetFPS(5);
-
+    //Start drawing
     BeginDrawing();
-
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
 
-        //read input
+        //Check for input down
         if (IsKeyDown(KEY_DOWN))
         {
             std::cout << "Input Detected: Down \n";
             MoveDown();
         }
-
+        //Check for input left
         if (IsKeyDown(KEY_LEFT))
         {
             std::cout << "Input Detected: Left \n";
             MoveLeft();
         }
-
+        //Check for input right
         if (IsKeyDown(KEY_RIGHT))
         {
             std::cout << "Input Detected: Right \n";
             MoveRight();
         }
-
+        //Check for input up/rotate
         if (IsKeyDown(KEY_UP))
         {
             std::cout << "Input Detected: Up \n";
             RotateRight();
         }
 
-        //if no piece add piece
-        if (piecePosX == -1 && piecePosY == -1)
+        //if no piece on the board add a piece
+        if (piecePosX == -100 && piecePosY == -100)
         {
             piecePosX = bCols / 2;
             piecePosY = 0 - (pieceHeight / 2);
             std::cout << "No Piece found, adding piece at: " << piecePosX << ", " << piecePosY << "\n";
             AddPieceToBoard(piecePosX, piecePosY);
-            DrawBoard();
         }
         DrawBoard();
-        CheckFullRow();
+        //CheckFullRow();
         //MoveDown();
         EndDrawing();
     }
@@ -151,14 +151,14 @@ void RemovePiece(int xPos, int yPos)
         }
     }
     //reset piece pos
-    piecePosX = -1;
-    piecePosY = -1;
+    piecePosX = -100;
+    piecePosY = -100;
 }
 
 void DrawBoard()
 {
     //draw board
-    std::cout << "drawing board \n";
+    //std::cout << "drawing board \n";
     for (int r = 0; r < bRows; r++)
     {
         for (int c = 0; c < bCols; c++)
@@ -175,7 +175,7 @@ void DrawBoard()
     }
 }
 
-void CheckFullRow()
+bool CheckFullRow()
 {
     std::cout << "Checking for full row \n";
     //check for full row 
@@ -218,35 +218,68 @@ void CheckFullRow()
         {
             board[0][c] = 0;
         }
-    }    
+        return true;
+    }
+
+    return false;
 }
 
 bool CheckMoveDown()
 {
-    bool canMove = true;
+    //bool canMove = true;
 
-    if (piecePosY > (bRows - pieceHeight))
-    {
-        return false;
-    }
+    //if (piecePosY > (bRows - pieceHeight))
+    //{
+    //    return false;
+    //}
 
-    //for each index of peice
-    for (int r = 0; r < pieceHeight; r++)
+    ////for each index of peice
+    //for (int r = pieceHeight - 1; r >= 0; r--)
+    //{
+    //    for (int c = 0; c < pieceWidth; c++)
+    //    {
+    //        //if bottom of piece
+    //        if (piece[r][c] != 0 && piece[r + 1][c] == 0)
+    //        {
+    //            //check space on board underneath
+    //            if (board[piecePosY + r + 1][piecePosX + c] != 0)
+    //            {
+    //                canMove = false;
+    //            }
+    //        }
+    //    }
+    //}
+    //return canMove;
+
+    //for each col
+    for (int r = pieceHeight - 1; r > 0; r--)
     {
+        //for each row
         for (int c = 0; c < pieceWidth; c++)
         {
-            //if bottom of piece
-            if (piece[r][c] != 0 && piece[r + 1][c] == 0)
+            //if piece found
+            if (piece[r][c] != 0)
             {
-                //check space on board underneath
-                if (board[piecePosY + r + 1][piecePosX + c] != 0)
+                if (piecePosY + r == bRows - 1)
                 {
-                    canMove = false;
+                    std::cout << "piece at edge of board. returning false \n";
+                    return false;
+                }
+                //check if edge
+                if (r == pieceHeight || piece[r + 1][c] == 0)
+                {
+                    //if space on board to left
+                    if (board[piecePosY + r + 1][piecePosX + c] != 0)
+                    {
+                        std::cout << "no space next to piece, returning false \n";
+                        return false;
+                    }
                 }
             }
         }
     }
-    return canMove;
+
+    return true;
 }
 
 bool CheckMoveLeft()
@@ -358,8 +391,12 @@ void MoveDown()
     }
     else
     {
-        piecePosX = -1;
-        piecePosY = -1;
+        piecePosX = -100;
+        piecePosY = -100;
+        while (CheckFullRow())
+        {
+            //row removed
+        }
     }
 }
 
@@ -399,8 +436,10 @@ void RotateRight()
     int posX = piecePosX;
     int posY = piecePosY;
     RemovePiece(piecePosX, piecePosY);
-    std::cout << "Rotating piece";
+    std::cout << "Rotating piece \n";
     int pieceSize = pieceHeight;
+    std::cout << "Piece at start \n";
+    PrintPiece();
     for (int r = 0; r < pieceSize; r++) 
     {
         for (int c = 0; c < pieceSize - r; c++)
@@ -410,6 +449,9 @@ void RotateRight()
             piece[pieceSize - 1 - c][pieceSize - 1 - r] = temp;
         }
     }
+    std::cout << "piece step 1 \n";
+    PrintPiece(); 
+    //flip top/bottom
     for (int r = 0; r < pieceSize / 2; r++) {
         for (int c= 0; c < pieceSize; c++) {
             int ptr = piece[r][c];
@@ -417,11 +459,20 @@ void RotateRight()
             piece[pieceSize - 1 - r][c] = ptr;
         }
     }
+    std::cout << "piece at end \n";
+    PrintPiece(); 
     AddPieceToBoard(posX, posY);
     DrawBoard();
 }
 
-void RotateLeft()
+void PrintPiece()
 {
-
+    for (int row = 0; row < pieceHeight; row++)
+    {
+        for (int col = 0; col < pieceWidth; col++)
+        {
+            std::cout << piece[row][col] << ", ";
+        }
+        std::cout << " \n";
+    }
 }
